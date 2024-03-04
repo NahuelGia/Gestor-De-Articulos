ae7dadb19b2d2a8ffe217fd5057005369a6611b2
Tomas-Neira-Guitera <tomasneira181@gmail.com>
2024-03-04 20:15:04 -0300

    Initial commit

---
+++ dominio/Articulo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Articulo
+    {
+
+        private decimal precio;
+        public int Id { get; set; }
+
+        [DisplayName("Descripción")]
+        public string Descripcion { get; set; }
+
+        public string Nombre { get; set; }
+        [DisplayName("Código")]
+        public string Codigo { get; set; }
+        public Marca Marca { get; set; }
+        [DisplayName("Categoría")]
+        public Categoria Categoria { get; set; }
+        public string UrlImagen { get; set; }
+
+        public decimal Precio 
+        {
+            get { return precio; }
+            set { precio = Math.Round(value, 2); }
+        }
+
+    }
+
+
+
+}
---
+++ negocio/ArticuloNegocio.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+
+namespace negocio
+{
+    public class ArticuloNegocio
+    {
+        public List<Articulo> listar() 
+        {
+            List<Articulo> listaDeArticulos = new List<Articulo>();
+            AccesoADatos accesoADatos = new AccesoADatos();
+
+            try
+            {
+                accesoADatos.asignarConsulta("select A.Id, codigo, nombre, A.Descripcion , M.Descripcion Marca, M.Id IdMarca, C.Descripcion Categoria, C.Id IdCategoria, ImagenUrl, Precio from ARTICULOS A join MARCAS M on A.IdMarca = M.Id join CATEGORIAS C on A.IdCategoria = C.Id");
+                accesoADatos.ejecutarLectura();
+                while(accesoADatos.Lector.Read()) 
+                {
+                    Articulo nuevoArticulo = new Articulo();
+                    Marca marcaDelArticulo = new Marca();
+                    Categoria categoriaDelArticulo = new Categoria();
+
+                    nuevoArticulo.Id = (int)accesoADatos.Lector["Id"];
+                    nuevoArticulo.Codigo = (string)accesoADatos.Lector["codigo"];
+                    nuevoArticulo.Nombre = (string)accesoADatos.Lector["nombre"];
+                    nuevoArticulo.Descripcion = (string)accesoADatos.Lector["Descripcion"];
+                    nuevoArticulo.Marca = marcaDelArticulo;
+                    nuevoArticulo.Marca.Id = (int)accesoADatos.Lector["IdMarca"];
+                    nuevoArticulo.Marca.Descripcion = (string)accesoADatos.Lector["Marca"];
+                    nuevoArticulo.Categoria = categoriaDelArticulo;
+                    nuevoArticulo.Categoria.Id = (int)accesoADatos.Lector["IdCategoria"];
+                    nuevoArticulo.Categoria.Descripcion = (string)accesoADatos.Lector["Categoria"];
+                    nuevoArticulo.UrlImagen = (string)accesoADatos.Lector["ImagenUrl"];
+                    nuevoArticulo.Precio = (decimal)accesoADatos.Lector["Precio"];
+
+                    listaDeArticulos.Add(nuevoArticulo);
+                }
+                return listaDeArticulos;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally 
+            {
+                accesoADatos.cerrarConexion();
+            }
+
+        }
+
+
+    }
+}
